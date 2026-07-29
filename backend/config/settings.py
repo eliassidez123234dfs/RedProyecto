4edec756a2314ba359938ca2935233c0bf7d36ce
Elias <hurtadoelias025@gmail.com>
2026-07-29 12:38:49 -0500

    chore: Union en el backend de las apps, users y landing para estar añadido en settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.landing',
+    'apps.users',
     'apps.models3d',
 ]
 
